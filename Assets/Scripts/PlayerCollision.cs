543b7875fd44b380f11efd3a5c1f8601fd62f72a
Lagical <70884073+Lagical@users.noreply.github.com>
2022-12-14 19:18:05 +0200

    Player collision script added

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Damage"))
+        {
+            DamagePlayer();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Damage"))
+        {
+            DamagePlayer();
+        }
+    }
+
+    private void DamagePlayer()
+    {
+        Debug.Log("hit!");
+    }
+
+}
